fbf17438a8bff560cbf0aeb8292d3acfa462d9ce
isaac <chijw@foxmail.com>
2023-09-13 00:45:47 +0800

    3d pca visualization complete

--- pca3dgraph.cpp
+++ pca3dgraph.cpp
@@ -1,5 +1,6 @@
 #include "pca3dgraph.h"
 #include "ui_pca3dgraph.h"
+#include "newtableview.h"
 //Eigen库
 #include "pca.hpp"
 #include "Eigen/Dense"
@@ -14,7 +15,6 @@ PCA3DGraph::PCA3DGraph(const CsvData& input, const std::vector<int>& col, QWidge
     scatter = new Q3DScatter;
     //从整体数据中筛选出选中的数据
     std::vector<std::vector<float>> originMatrix;
-    std::vector<bool> attr;
     for (const auto & p : data.AllPerson())
     {
         std::vector<float> originDot;
@@ -30,15 +30,25 @@ PCA3DGraph::PCA3DGraph(const CsvData& input, const std::vector<int>& col, QWidge
             }
         }
         originMatrix.emplace_back(originDot);
-        attr.emplace_back(data.MapList()[p.Diagnosis()]);
     }
     auto res = pca(originMatrix, 3);
-    QScatter3DSeries * bScatterSeries = new QScatter3DSeries;
-    bScatterSeries->setBaseColor(QColor(0,0,255,120));
-    QScatter3DSeries * mScatterSeries = new QScatter3DSeries;
-    mScatterSeries->setBaseColor(QColor(255,0,0,120));
-    QScatterDataArray bDataArray;
-    QScatterDataArray mDataArray;
+    int k = 0;
+    for (int i = 0; i < data.TypeList().size(); ++i)
+    {
+        k = std::max(k, data.TypeList().at(i));
+    }
+    k += 1;
+    std::vector<QScatter3DSeries *> seriesList;
+    std::vector<QScatterDataArray > arrayList;
+    for (int i = 0; i < k; ++i)
+    {
+        QScatter3DSeries * series = new QScatter3DSeries;
+        series->setBaseColor(NewTableView::colorList[i]);
+        seriesList.emplace_back(series);
+        QScatterDataArray array;
+        arrayList.emplace_back(array);
+
+    }
     //初始化数轴边界
     float xMinValue = res(0,0), xMaxValue = res(0,0);
     float yMinValue = res(0,1), yMaxValue = res(0,1);
@@ -47,15 +57,7 @@ PCA3DGraph::PCA3DGraph(const CsvData& input, const std::vector<int>& col, QWidge
     {
         QScatterDataItem item;
         item.setPosition(QVector3D(res(i,0),res(i,1),res(i,2)));
-        if (!attr[i])
-        {
-            bDataArray.append(item);
-        }
-        else
-        {
-            mDataArray.append(item);
-        }
-
+        arrayList[data.TypeList().at(i)].append(item);
         xMinValue = std::min(xMinValue, res(i, 0));
         xMaxValue = std::max(xMaxValue, res(i, 0));
         yMinValue = std::min(yMinValue, res(i, 1));
@@ -76,14 +78,14 @@ PCA3DGraph::PCA3DGraph(const CsvData& input, const std::vector<int>& col, QWidge
     scatter->setAxisX(axisX);
     scatter->setAxisY(axisY);
     scatter->setAxisZ(axisZ);
-    bScatterSeries->dataProxy()->addItems(bDataArray);
-    mScatterSeries->dataProxy()->addItems(mDataArray);
-    //设置点的大小
-    bScatterSeries->setItemSize(0.1);
-    mScatterSeries->setItemSize(0.1);
-    //scatterSeries加入scatter
-    scatter->addSeries(bScatterSeries);
-    scatter->addSeries(mScatterSeries);
+    for (int i = 0; i < seriesList.size(); ++i)
+    {
+        seriesList[i]->dataProxy()->addItems(arrayList[i]);
+        //设置点的大小
+        seriesList[i]->setItemSize(0.1);
+        //scatterSeries加入scatter
+        scatter->addSeries(seriesList[i]);
+    }
     //布局设置
     QVBoxLayout *layout = new QVBoxLayout(this);
     QWidget * container = QWidget::createWindowContainer(scatter);
